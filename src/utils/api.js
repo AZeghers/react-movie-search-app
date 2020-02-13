0b34459c539f13a5bdd9bdbd21d033349a8ae10a
Alexis Zeghers <42282939+AZeghers@users.noreply.github.com>
2020-02-13 06:36:52 +0100

    Adding a try/catch to look slightly more professional

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,10 +4,15 @@ const apiKey = 'f3a05026119d09f84c9aaef927a18ac2'
 
 const fetchMovieList = async (query) => {
 	const apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`
-	const res = await axios.get(apiUrl)
-	return res.data.results.filter((movie) =>
-		movie.original_title.toLowerCase().includes(query.toLowerCase()),
-	)
+	try {
+		const res = await axios.get(apiUrl)
+		return res.data.results.filter((movie) =>
+			movie.original_title.toLowerCase().includes(query.toLowerCase()),
+		)
+	} catch (err) {
+		console.error(err)
+		return []
+	}
 }
 
 export default { fetchMovieList }
