44abf48b2324cd1fd3e2a8847c2494bff83e9aa3
Alexis Zeghers <42282939+AZeghers@users.noreply.github.com>
2020-03-08 04:58:42 +0100

    Remove whoopsie

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const apiKey = 'f3a05026119d09f84c9aaef927a18ac2'
+const apiKey = 'YOUR API KEY HERE'
 
 const fetchMovieList = async (query) => {
 	const apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`
